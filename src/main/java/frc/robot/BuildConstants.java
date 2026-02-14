5492879ad7de24ff87dca61ed6c0fce908fafbfd
OkRobotsTeam <130770843+OkRobotsTeam@users.noreply.github.com>
2026-02-13 19:29:10 -0500

    02/13: tuned PID

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-8708-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "7d3120e33e7c8c1d90a3c4a6cb0df664e69fdd78";
-  public static final String GIT_DATE = "2026-02-09 19:32:59 EST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "388cb0b2eaacf107ad15a69a527a9eb15d5fd488";
+  public static final String GIT_DATE = "2026-02-11 19:33:37 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-02-11 19:16:32 EST";
-  public static final long BUILD_UNIX_TIME = 1770855392445L;
+  public static final String BUILD_DATE = "2026-02-13 19:07:38 EST";
+  public static final long BUILD_UNIX_TIME = 1771027658106L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
